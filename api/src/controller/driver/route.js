f1801ff2838d1274c00266a59ed397490360c063
Sanjay Yadav <sanjayyadav19298@gmail.com>
2022-02-14 19:01:33 +0530

    all admin validations with "validateAnyOneAdmin" function

--- api/src/controller/driver/route.js
+++ api/src/controller/driver/route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import { validateAnyOneAdmin } from "../../middleware/validateAdmin";
 import DriverController from "./DriverController";
 import { driverValidation } from "./_Validations";
 
 const router = Router({ mergeParams: true });
 
-router.get('/list', DriverController.list);
+router.get('/list', (req, res, next) => {validateAnyOneAdmin(req, res, next, 15)}, DriverController.list);
 router.post('/save', driverValidation, DriverController.save);
 router.delete("/delete/:id", DriverController.delete);
 
--- api/src/middleware/validateAdmin.js
+++ api/src/middleware/validateAdmin.js
@@ -1,67 +1,16 @@
-export const validateSuperAdmin = async (req, res, next) => {
-    try {
-        const cuser = req.__cuser;
-        if (cuser.type === "superAdmin") {
-            next();
-        } else {
-            throw new Error("Unauthorized");
-        }
-
-    } catch (error) {
-        res.status(401).json({ message: "Unauthorized" });
-    }
-};
-
-export const validateStateAdmin = async (req, res, next) => {
-    try {
-        const cuser = req.__cuser;
-        if (cuser.type === "stateAdmin") {
-            next();
-        } else {
-            throw new Error("Unauthorized");
-        }
-
-    } catch (error) {
-        res.status(401).json({ message: "Unauthorized" });
-    }
-};
-
-export const validateDistrictAdmin = async (req, res, next) => {
-    try {
-        const cuser = req.__cuser;
-        if (cuser.type === "districtAdmin") {
-            next();
-        } else {
-            throw new Error("Unauthorized");
-        }
-
-    } catch (error) {
-        res.status(401).json({ message: "Unauthorized" });
-    }
-};
-
-export const validateTehsilAdmin = async (req, res, next) => {
-    try {
-        const cuser = req.__cuser;
-        if (cuser.type === "tehsilAdmin") {
-            next();
-        } else {
-            throw new Error("Unauthorized");
-        }
-
-    } catch (error) {
-        res.status(401).json({ message: "Unauthorized" });
-    }
-};
+export const validateSuperAdmin = async (req, res, next) => { validateAnyOneAdmin(req, res, next, 8); }
+export const validateStateAdmin = async (req, res, next) => { validateAnyOneAdmin(req, res, next, 4); }
+export const validateDistrictAdmin = async (req, res, next) => { validateAnyOneAdmin(req, res, next, 2); }
+export const validateTehsilAdmin = async (req, res, next) => { validateAnyOneAdmin(req, res, next, 1); }
 
 
 export const validateAnyOneAdmin = async (req, res, next, num) => {
 
     /* 
-     * For Super Admin add 8
-     * For State Admin add 4
-     * For District Admin add 2
-     * For Taluk Admin add 1
+     * For Super Admin          add 8
+     * For State Admin          add 4
+     * For District Admin       add 2
+     * For Taluk Admin          add 1
      */
 
     num = ("0000" + ((num >>> 0).toString(2)));
