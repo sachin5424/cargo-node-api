6cee55afee173a8219400f9247ee2967d69594eb
Sanjay Yadav <arr0026@arramton.co.in>
2022-03-10 15:45:33 +0530

    bable js

--- api/app.js
+++
@@ -1,6 +0,0 @@
-import App from "./src/server";
-import config from "./src/utls/config";
-
-
-const app = new App(config.port);
-app.listen();
---
+++ api/src/app.js
@@ -0,0 +1,6 @@
+import App from "./server";
+import config from "./utls/config";
+
+
+const app = new App(config.port);
+app.listen();
