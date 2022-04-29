a9986376cca20a168efcc19273943e58fd51cfb2
satpnm <sat.web1989@gmail.com>
2022-04-29 12:21:18 +0530

    ss

---
+++ api/dist/app-test.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send("Root url")
+})
+
+app.listen(3005, (err)=>{
+    if(!err){
+        console.log('Server running on 3005');
+    } else{
+        console.log('Error while starting the server');
+        console.log('Reason:', err.message);
+    }
+})
